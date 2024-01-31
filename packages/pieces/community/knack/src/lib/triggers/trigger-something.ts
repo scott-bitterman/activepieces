d00c7e275300f7c9ae568ea2fd0e19e0697d3726
Scott Bitterman <8738895+scott-bitterman@users.noreply.github.com>
2024-01-31 12:36:48 -0500

    feat: prototyping trigger logic

--- packages/pieces/community/knack/src/lib/triggers/trigger-something.ts
+++ packages/pieces/community/knack/src/lib/triggers/trigger-something.ts
@@ -1,47 +1,111 @@
-/*
-export const createNewIssue = createTrigger({
-  auth: PieceAuth | undefined
-  name: string, // Unique name across the piece.
-  displayName: string, // Display name on the interface.
-description: string, // Description for the action
-  triggerType: POLLING | WEBHOOK,
+import { 
+  createTrigger, 
+  TriggerStrategy 
+} from "@activepieces/pieces-framework";
+import {
+  DedupeStrategy,
+  Polling,
+  pollingHelper,
+  httpClient, 
+  HttpMethod,
+} from '@activepieces/pieces-common';
+import { auth } from "../common/auth";
 
-  props: {}; // Required properties from the user.
-  // Run when the user enable or publish the flow.
+type AuthProps = {
+  apiKey: string;
+  applicationId: string;
+  subdomain?: string;
+};
 
-onEnable: (ctx) => {};
-  // Run when the user disable the flow or
-  // the old flow is deleted after new one is published.
-onDisable: (ctx) => {};
-  
-  // Trigger implementation, It takes context as parameter.
-  // should returns an array of payload, each payload considered
-  // a separate flow run.
-  run: async run(ctx): unknown[] => {}
-})
-*/
-import { createTrigger, Property, PieceAuth, TriggerStrategy } from "@activepieces/pieces-framework";
+type KnackResponse = {
+  records: any;
+  body: {
+    records: any[];
+  }
+};
+
+/*==============================
+  Polling Trigger
+  ==============================*/
+const polling: Polling<AuthProps, {}> = {
+  strategy: DedupeStrategy.LAST_ITEM,
+  items: async (context: any) => {
+    console.log('~~~polling context', JSON.stringify(context, null, 2));
+    const KNACK_URL = 'http://api.knackdev.com:3000/v1/objects/object_1/records?rows_per_page=10';
+
+		const response = await httpClient.sendRequest<KnackResponse>({
+			method: HttpMethod.GET,
+			url: KNACK_URL,
+			headers: {
+				// Local
+				'X-Knack-Application-Id': context.auth.applicationId,
+				'X-Knack-REST-API-Key': context.auth.apiKey,
+			},
+		});
+
+    console.log('~~~response.body.records', JSON.stringify(response.body.records, null, 2));
+
+    // This will return any new records
+    return response.body.records.map((record: any) => ({
+        id: record.id,
+        data: record,
+    }));
+  }
+}
 
 export const triggerSomething = createTrigger({
-  auth: undefined,
+  auth,
   name: 'knack-trigger', // Unique name across the piece.
-  displayName: 'knack-trigger', // Display name on the interface.
-  description: 'knack-trigger', // Description for the action
+  displayName: 'Record Insert', // Display name on the interface.
+  description: 'Triggers when a record is inserted into a Knack table', // Description for the action
   type: TriggerStrategy.POLLING,
 
   props: {}, // Required properties from the user.
 
 
   // Run when the user enable or publish the flow.
-  onEnable: async () => { },
+  onEnable: async (context) => { 
+    console.log('~~~~~~~enabled');
+    console.log('context enabled!', JSON.stringify(context, null, 2));
+    await pollingHelper.onEnable(polling, {
+      auth: context.auth,
+      store: context.store,
+      propsValue: context.propsValue,
+    });
+  },
+
   // Run when the user disable the flow or
   // the old flow is deleted after new one is published.
-  onDisable: async () => { },
+  onDisable: async (context) => { 
+    console.log('~~~~~~~disabled');
+    console.log('context disabled!', JSON.stringify(context, null, 2));
+    await pollingHelper.onDisable(polling, {
+      auth: context.auth,
+      store: context.store,
+      propsValue: context.propsValue,
+    });
+  },
 
   // Trigger implementation, It takes context as parameter.
   // should returns an array of payload, each payload considered
   // a separate flow run.
-  run: async () => [],
+  run: async (context) => {
+    console.log('~~~run context in trigger: ', JSON.stringify(context, null, 2));
+    return await pollingHelper.poll(polling, {
+      auth: context.auth,
+      store: context.store,
+      propsValue: context.propsValue,
+    });
+  },
+
+  test: async (context) => {
+    return await pollingHelper.test(polling, {
+      auth: context.auth,
+      store: context.store,
+      propsValue: context.propsValue,
+    });
+  },
+
   sampleData: {
     _id: '5f5e3e3e3e3e3e3e3e3e3e3e',
     value: 'hello world',
